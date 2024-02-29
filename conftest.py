53f68d390dacd6ff2d3c04395a171459dd1d6861
TomNicholas <tom@cworthy.org>
2024-02-29 15:37:15 -0500

    local test works

--- conftest.py
+++ conftest.py
@@ -1,14 +1,22 @@
-import contextlib
-import time
-import datetime
 import os
+import yaml
 
 import pytest
-import sqlalchemy
-import filelock
-import subprocess
 
 import cubed
+from cubed import config
+from cubed.spec import spec_from_config
+
+from typing import Iterator
+
+
+RUNTIME_CONFIGS = [
+    'configs/local_single-threaded.yaml'
+    #'configs/lithops_gcf.yaml'
+    #'configs/lithops_aws.yaml'
+    #'configs/lithops_aws_1Z.yaml'
+    #'configs/coiled_aws.yaml'
+]
 
 
 def run_benchmark(
@@ -16,27 +24,32 @@ def run_benchmark(
         executor, 
     ):
 
-    # TODO: cubed.compute won't yet work on an xarray object (like dask.compute does)
+    # TODO: cubed.compute won't yet work on an xarray object (like dask.compute does) because xarray has magic dask dunder methods
     cubed.compute(
         result, 
         executor=executor,
     )
 
 
-RUNTIME_CONFIGS = [
-    'local_single-threaded.yaml'
-    #'lithops_gcf.yaml'
-    #'lithops_aws.yaml'
-    #'lithops_aws_1Z.yaml'
-    #'coiled.yaml'
-]
+def spec_from_config_file(filepath: str) -> cubed.Spec:
+    # from https://donfig.readthedocs.io/en/latest/configuration.html#downstream-libraries
+    # TODO shouldn't there be a way to do this in cubed? i.e. a classmethod on the Spec object?
+
+    fn = os.path.join(os.path.dirname(__file__), filepath)
+
+    with open(fn) as f:
+        defaults = yaml.safe_load(f)
+
+    config.update_defaults(defaults)
+
+    return spec_from_config(config)
 
 
 @pytest.fixture(params=RUNTIME_CONFIGS)
-def runtime(config: str):
-    spec = cubed.spec.spec_from_config(config)
-
-    executor_name = ...
-    executor = cubed.runtime.create.create_executor(executor_name)
-
-    yield spec, executor
+def runtime(request) -> Iterator[cubed.Spec]:
+    """
+    Yields a cubed.Spec for each .yaml file in the /configs/ directory, 
+    so any test parametrized with this fixture will run for all executors.
+    """
+    config_filepath = request.param
+    yield spec_from_config_file(config_filepath)
